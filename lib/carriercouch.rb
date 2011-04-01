dfb44db3288fab208bb27c6170f85e57f5b0ad19
Shenouda Bertel <ShenoudaB@gmail.com>
2011-04-01 23:31:40 +0200

    add config

--- lib/carriercouch.rb
+++ lib/carriercouch.rb
@@ -1,18 +1,10 @@
 require 'carrierwave'
 require 'carriercouch/orm/couchrest_model'
 require 'carriercouch/storage/couch'
-#require 'carriercouch/uploader/configuration'
-
-#module CarrierWave
-#  module Storage
-#    autoload :Couch, 'carrierwave/storage/couch'
-#  end
-#end
+require 'carriercouch/uploader/configuration'
 
 CarrierWave.configure do |config|
   config.storage_engines[:couch] = 'CarrierWave::Storage::Couch'
-end
-
-module Carriercouch
-  # Your code goes here...
+  config.couch_host = 'localhost'
+  config.couch_port = 5984
 end
--- lib/carriercouch/uploader/configuration.rb
+++ lib/carriercouch/uploader/configuration.rb
@@ -13,13 +13,13 @@ module CarrierWave
         add_config :couch_connection
         add_config :couch_access_url
 
-        configure do |config|
-          config.storage_engines = {
-            :couch => "CarrierWave::Storage::Couch"
-          }
-          config.couch_host = 'localhost'
-          config.couch_port = 5984
-        end
+        #configure do |config|
+        #  config.storage_engines = {
+        #    :couch => "CarrierWave::Storage::Couch"
+        #  }
+        #  config.couch_host = 'localhost'
+        #  config.couch_port = 5984
+        #end
       end
     end
   end
